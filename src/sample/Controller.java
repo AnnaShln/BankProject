495a1dd8b41836cddfdfcc3c47cedc394699323e
Мария <mariapetrova.pmp@gmail.com>
2019-12-14 16:10:17 +0300

    hi3

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,19 +2,22 @@ package sample;
 
 public class Controller {
     class Person {
-        private string name;
+        private String name;
         private int id;
-        private list<string> tags;
-        private list<Project> projects;
+       // private List<String> tags;
+       // private list<Project> projects;
+
+        Person() {
+        }
     }
     class Project {
-        private string name;
+       // private string name;
         private int id;
-        private list<string> goals;
-        private list<string> tags;
-        private list<Person> performers;
+       // private list<string> goals;
+       // private list<string> tags;
+       // private list<Person> performers;
         private Person admin;
         private Person initiator;
-        private int test;
+        private int test1;
     }
 }
