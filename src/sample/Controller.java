5990b1dd3ff30e0edd952eadf6907f16066ab90b
Алиса Андреевна <sealisaa@yandex.ru>
2019-12-14 15:56:53 +0300

    hi2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,9 +2,18 @@ package sample;
 
 public class Controller {
     class Person {
-
+        private string name;
+        private int id;
+        private list<string> tags;
+        private list<Project> projects;
     }
     class Project {
-
+        private string name;
+        private int id;
+        private list<string> goals;
+        private list<string> tags;
+        private list<Person> performers;
+        private Person admin;
+        private Person initiator;
     }
 }
