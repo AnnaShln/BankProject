301bff475538ae737a6813aacc5aa0c32e84a13f
Алиса Андреевна <sealisaa@yandex.ru>
2019-12-20 19:24:06 +0300

    добавлены классы Person и Project

--- src/sample/classes/Person.java
+++ src/sample/classes/Person.java
@@ -1,4 +1,6 @@
 package sample.classes;
 
 public class Person {
+    private String name;
+    private int id;
 }
--- src/sample/classes/Project.java
+++ src/sample/classes/Project.java
@@ -1,4 +1,7 @@
 package sample.classes;
 
 public class Project {
+    private String name;
+    private int id;
+
 }
