22f224763e2a03bc99270a61caf8123c2d5bafc6
Алиса Андреевна <sealisaa@yandex.ru>
2019-12-20 20:13:43 +0300

    классы классы больше классов

--- src/sample/classes/Person.java
+++ src/sample/classes/Person.java
@@ -3,4 +3,30 @@ package sample.classes;
 public class Person {
     String name;
     int id;
+    //тут должен быть лист с проектами
+    //а еще лист с тегами
+    public String getName() {
+        return name;
+    }
+    public void setName(String p_name) {
+        this.name = p_name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int p_id) {
+        this.id = p_id;
+    }
+    class Student extends Person {
+        int course;
+        String group;
+        String inst; //институт (ИКНТ там и тд)
+        //метод вывода инфы по студенту
+    }
+    class Teacher extends Person {
+        String faculty;
+        String position;
+        //String subject;
+        //метод вывода инфы по преподу
+    }
 }
--- src/sample/classes/Project.java
+++ src/sample/classes/Project.java
@@ -3,4 +3,20 @@ package sample.classes;
 public class Project {
     String name;
     int id;
+    //тут лист с персонами
+    //а еще лист с тегами
+    public String getName() {
+        return name;
+    }
+    public void setName(String pr_name) {
+        this.name = pr_name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int pr_id) {
+        this.id = pr_id;
+    }
+    //тут метод вывода инфы по проекту
+    //метод по добавлению участников
 }
