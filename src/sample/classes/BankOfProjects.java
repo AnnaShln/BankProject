9a9912eb265cb334377f61ce62ab9c651e0d1491
Мария <mariapetrova.pmp@gmail.com>
2019-12-21 15:57:16 +0300

    Написала по строчке в нескольких функциях

--- src/sample/classes/BankOfProjects.java
+++ src/sample/classes/BankOfProjects.java
@@ -54,10 +54,9 @@ public class BankOfProjects {
         return taggedProjects;
     }
 
-    public void createProject(List<Project> list, String prName, Integer prId, String prPurpose, String prTasks,
+    public void createProject(String prName, Integer prId, String prPurpose, String prTasks,
                               String prDeadlines, String prResources) {
         Project project = new Project(prName,prId,prPurpose,prTasks,prDeadlines,prResources);
-        list.add(project);
+        allProjects.add(project);
     }
-
 }
